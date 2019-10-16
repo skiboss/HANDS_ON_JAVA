3799397a6ebfc27756666dac241b575a12483bc6
Meru Avwemoya <40310595+skiboss@users.noreply.github.com>
2019-10-16 16:29:29 +0100

    Add files via upload

---
+++ learn_variables.java
@@ -0,0 +1,10 @@
+public class MyProfile {
+	public static void main(String[] args) { 
+    String name = "Meru";
+    int age = 20;
+    double desiredSalary = 350000.00;
+    char gender = 'M';
+    boolean lookingForJob = false;
+
+	}
+}
